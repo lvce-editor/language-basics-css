9775741193800b580895d6483b9b4564517d0993
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-09-12 23:14:08 +0200

    use flat array return value (#7)

--- src/tokenizeCss.js
+++ src/tokenizeCss.js
@@ -104,12 +104,19 @@ export const initialLineState = {
   stack: [],
 }
 
+/**
+ * @param {any} lineStateA
+ * @param {any} lineStateB
+ */
 export const isEqualLineState = (lineStateA, lineStateB) => {
   return lineStateA.state === lineStateB.state
 }
 
+export const hasArrayReturn = true
+
 /**
  * @param {string} line
+ * @param {any} lineState
  */
 export const tokenizeLine = (line, lineState) => {
   let next = null
@@ -349,11 +356,9 @@ export const tokenizeLine = (line, lineState) => {
         console.log({ state, line })
         throw new Error('no')
     }
-    index += next[0].length
-    tokens.push({
-      type: token,
-      length: next[0].length,
-    })
+    const tokenLength = next[0].length
+    index += tokenLength
+    tokens.push(token, tokenLength)
   }
   return {
     state,
