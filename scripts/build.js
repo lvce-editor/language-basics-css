ca2a02e71d817a86047d6abd878a551ea6a9ca72
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2022-10-08 20:20:41 +0200

    update dependencies (#10)

--- scripts/build.js
+++ scripts/build.js
@@ -1,15 +1,22 @@
-import { copyFiles, packageExtension } from '@lvce-editor/package-extension'
-import path, { dirname, join } from 'node:path'
-import { fileURLToPath } from 'node:url'
+import { packageExtension } from '@lvce-editor/package-extension'
+import fs from 'fs'
+import path, { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
+
 const root = path.join(__dirname, '..')
 
-await copyFiles({
-  root,
-  files: ['README.md', 'extension.json', 'src'],
-  outDir: join(root, 'dist'),
-})
+fs.rmSync(join(root, 'dist'), { recursive: true, force: true })
+
+fs.mkdirSync(path.join(root, 'dist'))
+
+fs.copyFileSync(join(root, 'README.md'), join(root, 'dist', 'README.md'))
+fs.copyFileSync(
+  join(root, 'extension.json'),
+  join(root, 'dist', 'extension.json')
+)
+fs.cpSync(join(root, 'src'), join(root, 'dist', 'src'), { recursive: true })
 
 await packageExtension({
   highestCompression: true,
