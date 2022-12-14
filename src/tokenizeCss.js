bd861ae11aefb48c25602be90001d8d691d7fc33
Le Vivilet <levivlet@pm.me>
2022-12-14 11:16:25 +0100

    fix: highlighting of multiple property values

--- src/tokenizeCss.js
+++ src/tokenizeCss.js
@@ -342,6 +342,9 @@ export const tokenizeLine = (line, lineState) => {
         } else if ((next = part.match(RE_NUMERIC))) {
           token = TokenType.Numeric
           state = State.AfterPropertyValue
+        } else if ((next = part.match(RE_PROPERTY_VALUE_SHORT))) {
+          token = TokenType.CssPropertyValue
+          state = State.AfterPropertyValue
         } else if ((next = part.match(RE_PROPERTY_VALUE))) {
           token = TokenType.CssPropertyValue
           state = State.AfterPropertyValue
@@ -470,3 +473,5 @@ export const tokenizeLine = (line, lineState) => {
 // TODO test :hover, :after, :before, ::first-letter
 
 // TODO test complex background image url("data:image/svg+xml,%3Csvg%20xmlns%3D'http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg'%20viewBox%3D'0%200%206%203'%20enable-background%3D'new%200%200%206%203'%20height%3D'3'%20width%3D'6'%3E%3Cg%20fill%3D'%23b64e4e'%3E%3Cpolygon%20points%3D'5.5%2C0%202.5%2C3%201.1%2C3%204.1%2C0'%2F%3E%3Cpolygon%20points%3D'4%2C0%206%2C2%206%2C0.6%205.4%2C0'%2F%3E%3Cpolygon%20points%3D'0%2C2%201%2C3%202.4%2C3%200%2C0.6'%2F%3E%3C%2Fg%3E%3C%2Fsvg%3E")
+
+tokenizeLine(`body{  background-size: 17px 17px;`, initialLineState) //?
