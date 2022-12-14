a93b315935357e3a2918b698de24b360c502fb86
Le Vivilet <levivlet@pm.me>
2022-12-14 14:18:52 +0100

    fix: highlighting of variable with number

--- src/tokenizeCss.js
+++ src/tokenizeCss.js
@@ -87,7 +87,7 @@ const RE_SELECTOR_ID = /^#[\w\-\_]+/
 const RE_WHITESPACE = /^\s+/
 const RE_CURLY_OPEN = /^\{/
 const RE_CURLY_CLOSE = /^\}/
-const RE_PROPERTY_NAME = /^[a-zA-Z\-]+\b/
+const RE_PROPERTY_NAME = /^[a-zA-Z\-\w]+\b/
 const RE_COLON = /^:/
 const RE_PROPERTY_VALUE = /^[^;\}]+/
 const RE_PROPERTY_VALUE_SHORT = /^[^;\}\s\)]+/
@@ -525,3 +525,5 @@ export const tokenizeLine = (line, lineState) => {
 // TODO test :hover, :after, :before, ::first-letter
 
 // TODO test complex background image url("data:image/svg+xml,%3Csvg%20xmlns%3D'http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg'%20viewBox%3D'0%200%206%203'%20enable-background%3D'new%200%200%206%203'%20height%3D'3'%20width%3D'6'%3E%3Cg%20fill%3D'%23b64e4e'%3E%3Cpolygon%20points%3D'5.5%2C0%202.5%2C3%201.1%2C3%204.1%2C0'%2F%3E%3Cpolygon%20points%3D'4%2C0%206%2C2%206%2C0.6%205.4%2C0'%2F%3E%3Cpolygon%20points%3D'0%2C2%201%2C3%202.4%2C3%200%2C0.6'%2F%3E%3C%2Fg%3E%3C%2Fsvg%3E")
+
+tokenizeLine(`h1{  --color-2: rgb(0 0 0 / 2%);}`, initialLineState) //?
