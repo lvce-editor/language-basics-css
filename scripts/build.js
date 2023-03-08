d07b8f17e76b0e1c1dcd7e47debd5198dfec1348
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-03-08 20:53:35 +0100

    refactor: simplify build script (#64)

--- scripts/build.js
+++ scripts/build.js
@@ -1,26 +1,14 @@
-import { packageExtension } from '@lvce-editor/package-extension'
-import fs from 'fs'
-import path, { dirname, join } from 'path'
-import { fileURLToPath } from 'url'
+import { copyFiles, packageExtension } from '@lvce-editor/package-extension'
+import path, { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
-
 const root = path.join(__dirname, '..')
 
-fs.rmSync(join(root, 'dist'), { recursive: true, force: true })
-
-fs.mkdirSync(path.join(root, 'dist'))
-
-fs.copyFileSync(join(root, 'README.md'), join(root, 'dist', 'README.md'))
-fs.copyFileSync(
-  join(root, 'extension.json'),
-  join(root, 'dist', 'extension.json')
-)
-fs.copyFileSync(
-  join(root, 'languageConfiguration.json'),
-  join(root, 'dist', 'languageConfiguration.json')
-)
-fs.cpSync(join(root, 'src'), join(root, 'dist', 'src'), { recursive: true })
+await copyFiles({
+  root,
+  files: ['README.md', 'extension.json', 'languageConfiguration.json', 'src'],
+})
 
 await packageExtension({
   highestCompression: true,
